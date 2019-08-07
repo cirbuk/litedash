544771ccfe085e424e42ac961346c4fc67069a4f
Jophin Joseph <jophin.joseph88@gmail.com>
2019-08-07 13:05:27 +0530

    Moving to rollup-plugin-typescript2 because declaration files are not generated

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
-import typescript from 'rollup-plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import { terser } from "rollup-plugin-terser";
 import babel from "rollup-plugin-babel";
 
